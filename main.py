5b6de217a5ec6f1b04428d2aa9b7a689393614f0
Dairell <dairellhannan@yahoo.com>
2025-09-26 03:20:24 +0800

    GUIDELINES
    Python Library installation for functions

---
+++ main.py
@@ -0,0 +1,10 @@
+# =================================
+# 🚌 Bus Ticket Booking System
+# =================================
+# Guidelines:
+# 1. Install Pillow: pip install pillow
+# 2. Install qrcode: pip install qrcode
+# 3. Run : Click "run" button and click "start debugging" and choose python debugger
+
+from PIL import Image, ImageDraw, ImageFont
+import qrcode
