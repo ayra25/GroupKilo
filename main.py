2a3301cffdd708bcfcc34e5be16fd4f005ce6ce7
EzraGGIt <ezra2766@gmail.com>
2025-09-26 04:53:46 +0800

    added fares function for economy budget and premium
    bismillah boleh merge

--- main.py
+++ main.py
@@ -34,3 +34,9 @@ borneo_terminals = {
 peninsular_companies = ["Transnasional", "Plusliner", "MARA Liner", "KKKL Express"]
 borneo_companies = ["Borneo Express", "Sipitang Express", "Sungei Merah", "Miri Express"]
 
+fare_rates = {
+    "Budget": {"Standard": 0.11},
+    "Economy": {"Standard": 0.13, "VIP": 0.18},
+    "Premium": {"VIP": 0.22}
+}
+
