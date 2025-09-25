358124f2a275f0224a104c707b49826d33529ebf
Kadeesya <kadeesya2711@gmail.com>
2025-09-26 04:14:31 +0800

    added terminal bus around the peninsular area

--- main.py
+++ main.py
@@ -8,3 +8,19 @@
 
 from PIL import Image, ImageDraw, ImageFont
 import qrcode
+
+
+peninsular_terminals = {
+    "Kuala Lumpur": ["TBS (Terminal Bersepadu Selatan)"],
+    "Selangor": ["Shah Alam Terminal", "Klang Sentral"],
+    "Penang": ["Sungai Nibong Terminal", "Butterworth"],
+    "Perlis": ["Kangar Terminal"],
+    "Kedah": ["Alor Setar Terminal", "Sungai Petani Terminal"],
+    "Perak": ["Ipoh Amanjaya", "Taiping Terminal"],
+    "Negeri Sembilan": ["Seremban Terminal One"],
+    "Melaka": ["Melaka Sentral"],
+    "Johor": ["Larkin Sentral (Johor Bahru)", "Mersing Terminal"],
+    "Kelantan": ["Kota Bharu Terminal", "Tanah Merah Terminal"],
+    "Terengganu": ["Kuala Terengganu MBKT Terminal"],
+    "Pahang": ["Kuantan Sentral", "Temerloh Terminal"]
+}
