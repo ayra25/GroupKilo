2d55cebfb7c361678cd7f35a7cd057509a48a6e6
Dairell <dairellhannan@yahoo.com>
2025-09-26 17:57:07 +0800

    Updated choose region function
    System will restart if user put irrelevant input

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-# User has to install "pip install pillow qrcode" before running this system
-#To run this system, type "python3 bus_ticket.py" in terminal
+# =================================
+# 🚌 Bus Ticket Booking System
+# =================================
+# Guidelines:
+# 1. Install Pillow: pip install pillow
+# 2. Install qrcode: pip install qrcode
+# 3. Run : Click "run" button and click "start debugging" and choose python debugger
 
 from PIL import Image, ImageDraw, ImageFont
 import qrcode
@@ -8,11 +13,17 @@ print("========================================")
 print("      🚌 Bus Ticket Booking System")
 print("========================================\n")
 
-# Step 1: Ask region
-print("Choose Region:")
-print("1. Peninsular Malaysia")
-print("2. Borneo (Sabah & Sarawak)")
-region_choice = input("Enter choice (1/2): ")
+# Step 1 (Hannan) — choose_region()
+
+def choose_region():
+    while True:  # while loop to validate user input
+        print("Choose Region:")
+        print("1. Peninsular Malaysia")
+        print("2. Borneo (Sabah & Sarawak)")
+        region_choice = input("Enter choice (1/2): ")
+        if region_choice in ["1", "2"]:
+            return int(region_choice)
+        print("❌ Invalid choice, please enter 1 or 2.\n")
 
 # Step 2: Define terminals
 peninsular_terminals = {
